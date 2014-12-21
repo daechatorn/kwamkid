b9d770dc8f2cc620cfd3a924649ef66c33e8c8a8
daechatorn <daechatorn.man@gmail.com>
2014-12-21 17:57:13 +0700

    filter เสร็จจจจจจ

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -6,6 +6,10 @@ class dashboard extends CI_Controller{
 
 	public function index(){
 
+		$this->session->unset_userdata('topicsession');
+		$this->session->unset_userdata('dfromsession');
+		$this->session->unset_userdata('dtosession');
+
 		$this->load->view("dash/headernavbar");
 		
 
@@ -111,6 +115,131 @@ class dashboard extends CI_Controller{
 			exit();
 
 			
+
+	}
+
+
+	public function filter(){
+		$this->load->view("dash/headernavbar");
+
+		$qID = $this->input->post("qID");
+		$datefrom = $this->input->post("datefrom");
+		$dateto = $this->input->post("dateto");
+		
+		if($this->session->userdata('topicsession')=="" && $this->session->userdata('dtosession')=="" && $this->session->userdata('dfromsession')==""){
+			$ar = array(
+					"topicsession"=> $this->input->post("qID"),
+					"dfromsession"=>$this->input->post("datefrom"),
+					"dtosession"=>$this->input->post("dateto"),
+
+
+			);
+
+			$this->session->set_userdata($ar);
+		}
+
+		if(($this->session->userdata('topicsession')!=$qID  && $qID != "")
+			|| (($this->session->userdata('dtosession')!=$dateto && $dateto != "")
+			&& ($this->session->userdata('dfromsession')!=$datefrom && $datefrom != ""))){
+			$ar = array(
+					"topicsession"=> $this->input->post("qID"),
+					"dfromsession"=>$this->input->post("datefrom"),
+					"dtosession"=>$this->input->post("dateto"),
+
+
+			);
+
+			$this->session->set_userdata($ar);
+		}
+
+
+		$qID = $this->session->userdata('topicsession');
+		$datefrom = $this->session->userdata('dfromsession');
+		$dateto = $this->session->userdata('dtosession');
+
+		
+		$datecheck = "notchoose";
+		if($dateto != "" && $datefrom != ""){$datecheck="choose";}
+
+		$counttable = 0;
+		
+			if($datecheck == "choose" && $qID == "notchoose"){
+				$counttable = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("date >=",$datefrom)->where("date <=",$dateto)->count_all_results();
+			}
+			else if($datecheck == "notchoose" && $qID != "notchoose"){
+				$counttable = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("question.qID",$qID)->count_all_results();
+				
+			}
+			else if($datecheck == "choose" && $qID != "notchoose"){
+				$counttable = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("question.qID",$qID)->where("date >=",$datefrom)->where("date <=",$dateto)->count_all_results();
+			}
+			else{
+				$counttable = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->count_all_results();
+				
+			}
+		
+
+		//pagination
+			$config['base_url'] = base_url()."index.php/dashboard/filter";
+			$config['per_page'] = 5;
+			
+			$config['total_rows'] = $counttable;
+
+
+			
+
+			//out side
+			$config['full_tag_open'] = "<ul class='pagination'>";
+				
+				$config['first_tag_open'] = '<li>';
+				$config['first_tag_close'] = '</li>';
+
+   				$config['last_tag_open'] = '<li>';
+   				$config['last_tag_close'] = '</li>';
+
+				$config['prev_tag_open'] = '<li>';
+				$config['prev_tag_close'] = '</li>';
+
+				//current page
+				$config['cur_tag_open'] = "<li class='active'><a>";
+				$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+				
+				//another page
+				$config['num_tag_open'] = "<li>";
+				$config['num_tag_close'] = "</li>";
+
+				$config['next_tag_open'] = '<li>';
+				$config['next_tag_close'] = '</li>';
+
+			$config['full_tag_close'] = "</ul";
+
+			$this->pagination->initialize($config);
+
+
+
+			if($datecheck == "choose" && $qID == "notchoose"){
+				$data['comment'] = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("date >=",$datefrom)->where("date <=",$dateto)->limit($config['per_page'],end($this->uri->segments))->get()->result_array();
+			}
+			else if($datecheck == "notchoose" && $qID != "notchoose"){
+				$data['comment'] = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("question.qID",$qID)->limit($config['per_page'],end($this->uri->segments))->get()->result_array();
+				
+			}
+			else if($datecheck == "choose" && $qID != "notchoose"){
+				$data['comment'] = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->where("question.qID",$qID)->where("date >=",$datefrom)->where("date <=",$dateto)->limit($config['per_page'],end($this->uri->segments))->get()->result_array();
+			}
+			else{
+				$data['comment'] = $this->db->select('*')->from('question')->join('comment','question.qID = comment.qID')->limit($config['per_page'],end($this->uri->segments))->get()->result_array();
+				
+			}
+
+
+		
+		
+		$data['topic'] = $this->db->query("SELECT * FROM question ORDER By topic asc;")->result_array();
+
+		$this->load->view("dash/overview/filterpage",$data);
+		
+
 
 	}
 
---
+++ application/views/dash/overview/filterpage.php
@@ -0,0 +1,247 @@
+<div class="container-fluid" >
+      <div class="row">
+          <!--menu-->
+        <div class="col-sm-3 col-md-2 sidebar" style="margin-top:60px;">
+          <ul class="nav nav-sidebar">
+            <li style="background:rgba(255,155,202,0.5)"><a href="#">Overview</a></li>
+            <li ><a href="#">เพิ่มคำถาม</a></li>
+            <li><a href="#">แก้ไขคำถาม</a></li>
+            <li><a href="#">#</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">#</a></li>
+            <li><a href="">#</a></li>
+            <li><a href="">#</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">#</a></li>
+            <li><a href="">#</a></li>
+          </ul>
+        </div>
+
+    
+
+
+
+
+        <!--content-->
+        <div class="col-sm-9 " style="margin-top:30px;">
+          <h1 class="page-header">Dashboard</h1>
+          
+
+          <div class="row placeholders" style="margin-left:1px;margin-right:1px;">
+            <div class="col-sm-12" style="border:solid;border-color:black;padding:0;"> 
+              <div class="col-sm-8"  style="padding:4px;" > 
+                <div class="col-sm-4" style="padding:4px;">
+                  <h4 style="margin:0;padding:0;" class="sub-header">Comment Detail</h4>
+                </div>
+                <div class="col-sm-2" style="padding:4px;">
+                  <button type="button" class="btn btn-danger" onClick="deldata();" style="width:100%;" aria-label="Left Align">
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                  </button>
+
+                </div>
+              </div>
+              <div class="col-sm-2" style="padding:4px;">
+                <button class="btn btn-success exportbutton"  style="width:100%;">Export</button>
+              </div>
+              <div class="col-sm-2" style="padding:4px;">
+                <button class="btn btn-primary filterbutton"  style="width:100%;">Filter</button>
+              </div>
+            </div>
+            <script type="text/javascript">
+              $(document).ready(function(){
+                $("#exportbox").hide();
+                $("#filterbox").hide();
+                
+                $count = 1;
+                $(".exportbutton").click(function(){
+                  if($count == 1){
+                    $("#exportbox").show();
+                    $count = 0;
+                  }
+                  else{
+                    $("#exportbox").hide();
+                    $count = 1;
+                  }
+                });
+
+                $countfil =1;
+                $(".filterbutton").click(function(){
+                  if($countfil == 1){
+                    $("#filterbox").show();
+                    $countfil = 0;
+                  }
+                  else{
+                    $("#filterbox").hide();
+                    $countfil = 1;
+                  }
+                });
+
+              });
+            </script>
+
+            <?php echo form_open("dashboard/export");?>
+              <div class="col-sm-12" id="exportbox" style="border:solid;border-color:black;padding:1px;" >
+                <div class="col-sm-1 "  style="padding:0.5px 0 0.5px 0;" > 
+                  <h5 style="margin:7px;">Topic: </h5> 
+                </div>
+                <div class="col-sm-11 "  style="padding:0.5px 0 0.5px 0;" > 
+                  <select name="qID" class="form-control">
+                    <option value="notchoose">Not Choose</option>
+                    <?php 
+                      foreach ($topic as $key => $value) {
+                        echo "<option value='".$value['qID']."'>"."- ".$value['topic']."</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+
+                <div class="col-sm-1" style="padding:0px 0 0.5px 0;">
+                  <h5 style="margin:7px;">Date From: </h5> 
+                </div>
+                <div class="col-sm-4" style="padding:0.5px 0 0.5px 0;">
+                  <input type="date" name="datefrom" value="notchoose" class="form-control" style="width:100%;"/>
+                </div>
+                <div class="col-sm-1" style="padding:0.5px 0 0.5px 0;">
+                  <h5 style="margin:7px;">Date To: </h5> 
+                </div>
+                <div class="col-sm-4" style="padding:0.5px 0 0.5px 0;">
+                 <input type="date" name="dateto" value="notchoose" class="form-control" style="width:100%;"/>
+                </div>
+                <div class="col-sm-1" style="padding:0.5px 0 0.5px 0;">
+                  <input type="submit" class="form-control btn btn-info" style="margin-left:2px;" value="export" style="width:100%;"/>
+                </div>
+              </div>
+            <?php echo form_close();?>
+
+            <?php echo form_open("dashboard/filter");?>
+              <div class="col-sm-12" id="filterbox" style="border:solid;border-color:black;padding:1px;" >
+                <div class="col-sm-1 "  style="padding:0.5px 0 0.5px 0;" > 
+                  <h5 style="margin:7px;">Topic: </h5> 
+                </div>
+                <div class="col-sm-11 "  style="padding:0.5px 0 0.5px 0;" > 
+                  <select name="qID" class="form-control topicfilter">
+                    <option value="notchoose">Not Choose</option>
+                    <?php 
+                      foreach ($topic as $key => $value) {
+                        echo "<option value='".$value['qID']."'>"."- ".$value['topic']."</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+
+                <div class="col-sm-1" style="padding:0px 0 0.5px 0;">
+                  <h5 style="margin:7px;">Date From: </h5> 
+                </div>
+                <div class="col-sm-4" style="padding:0.5px 0 0.5px 0;">
+                  <input type="date" name="datefrom" value="notchoose" class="form-control" style="width:100%;"/>
+                </div>
+                <div class="col-sm-1" style="padding:0.5px 0 0.5px 0;">
+                  <h5 style="margin:7px;">Date To: </h5> 
+                </div>
+                <div class="col-sm-4" style="padding:0.5px 0 0.5px 0;">
+                 <input type="date" name="dateto" value="notchoose" class="form-control" style="width:100%;"/>
+                </div>
+                <div class="col-sm-1" style="padding:0.5px 0 0.5px 0;">
+                  <input type="submit" class="form-control btn btn-info" style="margin-left:2px;" value="filter" style="width:100%;"/>
+                </div>
+              </div>
+            <?php echo form_close();?>
+
+
+          </div>
+
+          <script type="text/javascript">
+            $(document).ready(function(){
+              alert("<?php echo $this->session->userdata('topicsession');?>");
+              alert("<?php echo $this->session->userdata('dfromsession');?>");
+              alert("<?php echo $this->session->userdata('dtosession');?>");
+            });
+          </script>
+
+
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Comment</th>
+                  <th>Question</th>
+                  <th>Respondents</th>
+                  <th>Date</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  if(count($comment)==0){
+                    echo "<tr>";
+                      echo "<td colspan='6' align='center'>"."---- no data ----"."</td>";
+                    echo "</tr>";
+                  }
+                  else{
+                    foreach ($comment as $key => $value) {
+                      echo "<tr class='rowcontent'>";
+                        echo "<td>";
+                        echo "<input type='checkbox' name='checkdelete' id='".$value['cID']."' value='".$value['cID']."' />";
+                        echo "</td>";
+
+                        echo "<td>";
+                        echo $value['detail'];
+                        echo "</td>";
+
+                        echo "<td>";
+                        echo $value['topic'];
+                        echo "</td>";
+
+                        echo "<td>";
+                        echo $value['firstname']." ".$value['lastname'];
+                        echo "</td>";
+
+                        echo "<td>";
+                        echo $value['date'];
+                        echo "</td>";
+
+                        echo "<td>";
+                        echo $value['time'];
+                        echo "</td>";
+                      echo "</tr>";
+                    }
+
+                  }
+                ?>
+              </tbody>                                                                      
+            </table>
+          </div>
+
+          <script type="text/javascript">
+            $(document).ready(function(){
+                  $(".rowcontent").mouseover(function(){
+                    $(this).css("background","rgba(253,52,249,0.3)");
+                  });
+                  $(".rowcontent").mouseout(function(){
+                    $(this).css("background","white");
+                  });
+            });
+          </script>
+
+
+          <?=$this->pagination->create_links();?>
+
+
+        </div>
+      </div>
+    </div>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?=base_url();?>assests/bootstrap/js/bootstrap.min.js"></script>
+    
+
+  </body>
+</html>
